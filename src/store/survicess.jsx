c4679bd2cb30c1afd2ba94fb0faac2ce60698e4e
Nihal-75 <nihalpandey636@gmail.com>
2025-11-22 15:06:59 +0530

    icons

--- src/store/survicess.jsx
+++ src/store/survicess.jsx
@@ -1,43 +1,49 @@
+import { FaGlobe, FaMobileAlt, FaPalette, FaRocket, FaTools } from "react-icons/fa";
+
 export const ServiceData = [
   {
     id: 1,
     title: "Mobile App Development",
-    icon: "FaMobileAlt",
+    icon: <FaMobileAlt/>,
+
+
+
     description:
       "I develop fast, scalable, and feature-rich mobile applications using Flutter and React Native. From UI design to backend integration, I build apps that provide a smooth and modern user experience.",
   },
   {
     id: 2,
     title: "Web Development",
-    icon: "FaGlobe",
+    icon: <FaGlobe/>,
     description:
       "Creating responsive, modern, and user-focused websites using HTML, CSS, JavaScript, React.js, Node.js, and MongoDB. I build everything from business websites to powerful full-stack applications.",
   },
   {
     id: 3,
     title: "UI / UX Design",
-    icon: "FaPalette",
+    icon: <FaPalette/>,
     description:
       "Designing simple, clean, and user-friendly interfaces using Figma. I focus on user experience, clarity, and elegant layouts that enhance product usability.",
   },
   {
     id: 4,
     title: "Backend Development & API Integration",
-    icon: "FaCode",
+    icon: <FaCode/>,
+
     description:
       "Building secure and scalable backend systems with Express.js and Node.js. I handle REST APIs, authentication, database management, and third-party integrations.",
   },
   {
     id: 5,
     title: "Website Performance & SEO Optimization",
-    icon: "FaRocket",
+    icon: <FaRocket/>,
     description:
       "Improving website speed, SEO score, loading time, and overall performance. I optimize images, scripts, and structure for a lightning-fast user experience.",
   },
   {
     id: 6,
     title: "Maintenance & Support",
-    icon: "FaTools",
+    icon: <FaTools/>,
     description:
       "Providing ongoing support, updates, improvements, bug fixes, and long-term maintenance to ensure your website or app runs smoothly with zero downtime.",
   },
