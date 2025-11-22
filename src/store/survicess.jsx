ac3c0d7495e97504578620dce3cab2517729358a
Nihal-75 <nihalpandey636@gmail.com>
2025-11-22 15:09:06 +0530

    icons

--- src/store/survicess.jsx
+++ src/store/survicess.jsx
@@ -1,4 +1,4 @@
-import { FaGlobe, FaMobileAlt, FaPalette, FaRocket, FaTools } from "react-icons/fa";
+import { FaCode, FaGlobe, FaMobileAlt, FaPalette, FaRocket, FaTools } from "react-icons/fa";
 
 export const ServiceData = [
   {
